bbeb68ad3fb4dc18b5bf75ce2a11dd7b4edf9fa5
babu <babu@babu.com>
2013-04-10 00:59:02 -0700

    before bug: simplified person by removing all relationships. created 30 persons and search now shows paged results at - http://localhost:8080/PersonWeb/people
    Queries for above search
    select count(*) as col_0_0_ from Person person0_ limit ?
    select person0_.id as id0_, person0_.name as name0_, person0_.version as version0_ from Person person0_ limit ?

--- src/main/java/org/springframework/data/rest/example/Person.java
+++ src/main/java/org/springframework/data/rest/example/Person.java
@@ -3,6 +3,7 @@ package org.springframework.data.rest.example;
 import java.util.List;
 import java.util.Map;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
@@ -18,25 +19,18 @@ public class Person {
   private String name;
   @Version
   private Long version;
-  @OneToMany
-  private List<Address> addresses;
-  @OneToMany
-  private Map<String, Profile> profiles;
+
 
   public Person() {
   }
 
-  public Person(Long id, String name, List<Address> addresses, Map<String, Profile> profiles) {
+  public Person(Long id, String name) {
     this.id = id;
     this.name = name;
-    this.addresses = addresses;
-    this.profiles = profiles;
   }
 
-  public Person(String name, List<Address> addresses, Map<String, Profile> profiles) {
+  public Person(String name) {
     this.name = name;
-    this.addresses = addresses;
-    this.profiles = profiles;
   }
 
   public Long getId() {
@@ -57,22 +51,6 @@ public class Person {
 
   public void setVersion(Long version) {
     this.version = version;
-  }
-
-  public List<Address> getAddresses() {
-    return addresses;
-  }
-
-  public void setAddresses(List<Address> addresses) {
-    this.addresses = addresses;
-  }
-
-  public Map<String, Profile> getProfiles() {
-    return profiles;
-  }
-
-  public void setProfiles(Map<String, Profile> profiles) {
-    this.profiles = profiles;
   }
 
 }
